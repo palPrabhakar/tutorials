0c14791d7be4e8b963cea75bd36eeb0518a3d632
llcc42 <prabhakar.pal00@gmail.com>
2019-01-23 00:11:49 +0530

    mpi send and receive tut

---
+++ mpi/dynamic_recieve/check_status.c
@@ -0,0 +1,15 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char **argv) 
+{
+    MPI_Init(NULL, NULL);
+
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+} 
---
+++ mpi/send_and_recieve/ping_pong.c
@@ -0,0 +1,33 @@
+#include <mpi.h>
+#include <stdio.h>
+
+#define PING_PONG_LIMIT 10
+
+int main(int argc, char **argv) 
+{
+    MPI_Init(NULL, NULL);
+
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    int ping_pong_count = 0;
+    int partner_rank = (world_rank + 1) % 2;
+    while(ping_pong_count < PING_PONG_LIMIT) {
+        if (world_rank == ping_pong_count % 2) {
+            ping_pong_count++;
+            MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
+            printf("%d Send and incremented ping pong count %d to %d\n", world_rank, ping_pong_count, partner_rank); 
+        } else {
+            MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("%d recieved ping pong count %d from %d\n", world_rank, ping_pong_count, partner_rank);
+        }
+    }
+
+    MPI_Finalize();
+
+    return 0;
+
+} 
