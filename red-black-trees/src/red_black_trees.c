d178b08f2aa339f33d58ac624b1c620931b17fae
Prabhakar Pal <prabhakar.pal00@gmail.com>
2024-10-21 00:06:35 +0530

    separate directory for queues

--- red-black-trees/src/red_black_trees.c
+++ red-black-trees/src/red_black_trees.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "queue.h"
+#include "../../queue/src/queue.h"
 #include "red_black_trees.h"
 
 #define CHECK_QUEUE(func, ...)                                                 \
